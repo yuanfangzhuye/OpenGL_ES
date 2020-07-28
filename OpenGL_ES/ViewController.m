0f08e77efa6ebfb4e66eb3a4dbd6b7c189c821c8
chao.li <chao.li@team-lab.cn>
2020-07-28 15:23:37 +0800

    update

--- OpenGL_ES/ViewController.m
+++ OpenGL_ES/ViewController.m
@@ -21,8 +21,13 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    //1.OpenGL ES 相关初始化
     [self setupConfig];
+    
+    //2.加载顶点/纹理坐标数据
     [self setupVertexDatas];
+    
+    //3.加载纹理数据(使用GLBaseEffect)
     [self setupTexture];
 }
 
@@ -42,6 +47,25 @@
     view.context = context;
     
     //3.配置视图创建的渲染缓存区
+    /**
+     1⃣️drawableColorFormat：颜色缓存区格式
+     OpenGL ES 有一个缓存区，它用以存储将在屏幕中显示的颜色。你可以使用其属性来设置缓冲区中的每个像素的颜色格式。
+     
+     GLKViewDrawableColorFormatRGBA8888 = 0,
+     默认.缓存区的每个像素的最小组成部分（RGBA）使用8个bit，（所以每个像素4个字节，4*8个bit）
+     
+     GLKViewDrawableColorFormatRGB565,
+     如果你的APP允许更小范围的颜色，即可设置这个。会让你的APP消耗更小的资源（内存和处理时间）
+     
+     2⃣️drawableDepthFormat: 深度缓存区格式
+     
+     GLKViewDrawableDepthFormatNone = 0,意味着完全没有深度缓冲区
+     GLKViewDrawableDepthFormat16,
+     GLKViewDrawableDepthFormat24,
+     如果你要使用这个属性（一般用于3D游戏），你应该选择GLKViewDrawableDepthFormat16
+     或GLKViewDrawableDepthFormat24。这里的差别是使用GLKViewDrawableDepthFormat16
+     将消耗更少的资源
+     */
     view.drawableColorFormat = GLKViewDrawableColorFormatRGBA8888;
     view.drawableDepthFormat = GLKViewDrawableDepthFormat24;
     
@@ -77,10 +101,31 @@
     glBindBuffer(GL_ARRAY_BUFFER, bufferID);
     
     //(3).将顶点数组的数据copy到顶点缓存区中(GPU显存中)
+    //参数1：目标
+    //参数2：坐标数据的大小
+    //参数3：坐标数据
+    //参数4：用途
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertexData), vertexData, GL_STATIC_DRAW);
     
-    //顶点坐标数据
+    //3. 打开读取通道
+    /**
+     ⚠️
+     在iOS中，出于性能考虑，所有顶点着色器的属性（Attribute）变量通道都是默认关闭的，这就意味着顶点数据在着色器端（服务端）是不可用的，即使你已经使用 glBufferData 方法，将顶点数据从内存拷贝到顶点缓存区中（GPU显存中）。所以，必须由 glEnableVertexAttribArray 方法打开通道，指定访问属性，才能让顶点着色器能够访问到从 CPU 复制到 GPU 的数据。
+     
+     数据在 GPU 端是否可见，即着色器能否读取到数据，由是否启用了对应的属性决定，这就是 glEnableVertexAttribArray 的功能，允许顶点着色器读取 GPU（服务器端）数据。
+     */
     glEnableVertexAttribArray(GLKVertexAttribPosition);
+    
+    //4. 上传顶点数据到显存的方法（设置合适的方式从buffer里面读取数据）
+    /**
+     参数1：传递顶点坐标的类型有五种类型：position[顶点]、normal[法线]、color[颜色]、texCoord0[纹理一]、texCoord1[纹理二]，这里用的是顶点类型
+     参数2：每次读取数量（如 position 是由3个（x,y,z）组成，而颜色是4个（r,g,b,a），纹理则是2个）
+     参数3：指定数组中每个组件的数据类型。可用的符号常量有GL_BYTE, GL_UNSIGNED_BYTE, GL_SHORT,GL_UNSIGNED_SHORT, GL_FIXED, 和 GL_FLOAT，初始值为GL_FLOAT
+     参数4：指定当被访问时，固定点数据值是否应该被归一化（GL_TRUE）或者直接转换为固定点值（GL_FALSE）
+     参数5：步长，取完一次数据需要跨越多少步长去读取下一个数据，如果为0，那么顶点属性会被理解为：它们是紧密排列在一起的。初始值为0
+     参数6：指定一个指针，指向数组中第一个顶点属性的第一个组件。初始值为0
+     */
+    // 上传顶点数据到显存（设置合适的方式从 buffer 里面读取数据）
     glVertexAttribPointer(GLKVertexAttribPosition, 3, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 5, (GLfloat *)NULL + 0);
     
     //纹理坐标数据
@@ -93,6 +138,10 @@
     //1.获取纹理图片路径
     NSString *filePath = [[NSBundle mainBundle] pathForResource:@"timg" ofType:@"png"];
     
+    /**
+    ⚠️注意：
+    iOS 的坐标计算是从左上角 [0, 0] 开始，到右下角 [1, 1]。但是在文里中的原点不是左上角，而是左下角 [0, 0]，右上角[1, 1]。所以如果想要正确的加载图片，需要设置纹理的原点为左下角。否则得到的图片将会是一张倒立的图片。
+    */
     //2.设置纹理参数(纹理坐标原点是左下角，但是图片显示原点应该是左上角)
     NSDictionary *options = [NSDictionary dictionaryWithObjectsAndKeys:@(1), GLKTextureLoaderOriginBottomLeft, nil];
     GLKTextureInfo *textureInfo = [GLKTextureLoader textureWithContentsOfFile:filePath options:options error:nil];
